be4169e1e77c18621167a7447a26fb429a320f2b
Kishore <126963491+Kishore-67@users.noreply.github.com>
2025-04-06 14:47:26 +0530

    Update Homes.js

--- src/components/Pages/Homes.js
+++ src/components/Pages/Homes.js
@@ -6,7 +6,6 @@ import './Homes.css';
 const Home = ({ title, content }) => {
   return (
     <div className="Home">
-      <p>HI 👋</p>
       <p>Visiting the Best Planner for your adventurous trip</p>
     </div>
 
